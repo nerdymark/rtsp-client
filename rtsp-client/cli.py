2ff426ee741a87eee2fe0ab4f11f3ab69c70d80c
Mark LaCore <nerdymark@gmail.com>
2020-11-14 12:45:35 -0800

    Initial commit.

---
+++ rtsp-client/cli.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for rtsp-client."""
+import logging
+import os
+import sys
+import rtsp
+import time as t
+
+rtspUser = os.environ['RTSP_USER']
+rtspPass = os.environ['RTSP_PASS']
+previewLength = 15
+
+cameraList = [
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch01/0',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch02/0',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch03/0',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch04/0',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.152:554/ch01/0',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.152:554/ch02/0'
+]
+
+while True:
+    for url in cameraList:
+        with rtsp.Client(url) as client:
+            print('Trying ' + url)
+            previewEnd = t.time() + previewLength
+            while t.time < previewEnd:
+                try:
+                    client.preview()
+                except Exception as e:
+                    print(e)
+                    pass
