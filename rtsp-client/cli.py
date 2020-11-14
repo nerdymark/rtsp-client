fb060b0a1851f11fb8ce5aa6e15ab1880b31cb60
Mark LaCore <nerdymark@gmail.com>
2020-11-14 13:06:25 -0800

    fixes

--- rtsp-client/cli.py
+++ rtsp-client/cli.py
@@ -12,12 +12,12 @@ rtspPass = os.environ['RTSP_PASS']
 previewLength = 15
 
 cameraList = [
-    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch01/0',
-    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch02/0',
-    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch03/0',
-    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch04/0',
-    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.152:554/ch01/0',
-    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.152:554/ch02/0'
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch01/1',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch02/1',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch03/1',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch04/1',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.152:554/ch01/1',
+    'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.152:554/ch02/1'
 ]
 
 while True:
@@ -25,9 +25,10 @@ while True:
         with rtsp.Client(url) as client:
             print('Trying ' + url)
             previewEnd = t.time() + previewLength
-            while t.time < previewEnd:
+            while t.time() < previewEnd:
                 try:
                     client.preview()
                 except Exception as e:
                     print(e)
                     pass
+        client.close()
