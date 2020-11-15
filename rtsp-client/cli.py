eb913b8020b232515bd60b74aecc512b909f818d
Mark LaCore <nerdymark@gmail.com>
2020-11-14 16:20:10 -0800

    fixes

--- rtsp-client/cli.py
+++ rtsp-client/cli.py
@@ -6,25 +6,11 @@ import os
 import sys
 import rtsp
 import time
+from interruptingcow import timeout
+
 
 rtspUser = os.environ['RTSP_USER']
 rtspPass = os.environ['RTSP_PASS']
-
-def previewLength(max_seconds, *, interval=1):
-    interval = int(interval)
-    start_time = time.time()
-    end_time = start_time + max_seconds
-    yield 0
-    while time.time() < end_time:
-        if interval > 0:
-            next_time = start_time
-            while next_time < time.time():
-                next_time += interval
-            time.sleep(int(round(next_time - time.time())))
-        yield int(round(time.time() - start_time))
-        if int(round(time.time() + interval)) > int(round(end_time)): 
-            return
-
 
 cameraList = [
     'rtsp://'+rtspUser+':'+rtspPass+'@10.0.1.150:554/ch01/1',
@@ -37,7 +23,10 @@ cameraList = [
 
 while True:
     for url in cameraList:
-        for sec in previewLength(15):
-            with rtsp.Client(url) as client:
-                client.preview()
-            client.close()
+        try:
+            with timeout(5, exception=RuntimeError):
+                with rtsp.Client(url) as client:
+                    client.preview()
+                client.close()
+        except RuntimeError:
+            pass
